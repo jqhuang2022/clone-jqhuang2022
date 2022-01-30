4df966a8c060f41bab2f55f776415e8a46765a46
jqhuang2022 <97681599+jqhuang2022@users.noreply.github.com>
2022-01-30 18:07:29 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
 #include<stdio.h>
+struct dataset
+{
+    int datayip;//源ip
+    int datayport;//源端口
+    int datamip;//目的ip
+    int datamport;//目的端口
+    int datapro;//协议
+};
+struct ruleset
+{
+    char ruleyip[50];//源IP
+    char ruleyport[50];//源端口
+    char rulemip[50];//目的IP
+    char rulemport[50];//目的端口
+    int rulepro;//协议
+}
 int main()
 {
-    int data,data1,result1,result2,result3,result;
-    freopen("packet1.txt","r",stdin);
-    freopen("result1.txt","w",stdout);
+    int data,rule1,rule2;
+    int result1,result2,result3,result4,result5,result;
+    int rule;
+    freopen("packet1","r",stdin);
+    freopen("result1","w",stdout);
     while(scanf("%d",&data)!=EOF)
     {
-        data1=change(data);
-        result1=ip(data1);
-        result2=port(data1);
-        result3=pro(data1);
-        result=conclue(result1,result2,result3);
+        rule1=change(ruleset.ruleyip);
+        rule2=change(ruleset.rulemip);
+        result1=ip(dataset.datayip,rule1);
+        result2=ip(dataset.datamip,rule2);
+        result3=port(dataset.datayport,ruleset.ruleyport);
+        result4=port(dataset.datamport,ruleset.rulemport);
+        result5=pro(dataset.datapro,ruleset.rulepro);
+        result=conclue(result1,result2,result3,result4,result5);
         printf("%d",result);
     }
     fclose(stdin);
     fclose(stdout);
     return 0;
 }
-void change(int data) //将数据IP地址从十进制转化为点分十进制
-int ip(int data)//规则和数据IP地址匹配
-int port(int data)//端口匹配
-int pro(int data) //协议匹配
-int conclude(int result1,int result2,int result3)//输出结果
+void change(int rule) //将规则IP地址从点分十进制转化为十进制
+int ip(int data,int rule)//规则和数据IP地址匹配
+int port(int data,int rule)//端口匹配
+int pro(int data,int rule) //协议匹配
+int conclude(int result1,int result2,int result3,int result4,int result5)//输出结果
